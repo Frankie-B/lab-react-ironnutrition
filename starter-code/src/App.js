bcff45f77ad513e7f8181d5846510c500445b83f
Francis Bukenya <frankie.bukenya@gmail.com>
2020-04-05 10:50:49 +0200

    Add iteration II

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Food from './Food';
+import foods from './foods.json';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {foods.map((food) => {
+          return (
+            <Food
+              image={food.image}
+              name={food.name}
+              calories={food.calories}
+            />
+          );
+        })}
       </div>
     );
   }
